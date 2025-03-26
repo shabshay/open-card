528ae4144357e0c3b64606de370c17e6a5b8a161
sshabtay <shay.shabshay@gmail.com>
2025-03-26 09:11:40 +0200

    adding modal and tooltip

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
+import {NgClass} from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  imports: [
+    NgClass
+  ],
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements AfterViewInit {
+  @Input() title: string = '';
+  @Input() confirmText: string = 'סגור';
+  @Output() close = new EventEmitter<void>();
+
+  visible = true;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const el = document.querySelector('.modal-backdrop') as HTMLElement;
+      if (el) el.focus();
+    }, 0);
+  }
+
+  onClose() {
+    this.visible = false;
+    setTimeout(() => this.close.emit(), 300); // התאמה לזמן האנימציה
+  }
+}
