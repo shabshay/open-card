8f527e08cd896082b15fc02171f5b5822ebdbe2b
sshabtay <shay.shabshay@gmail.com>
2025-03-27 15:12:03 +0200

    adding header comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TruthOrDareComponent } from './games/truth-or-dare/truth-or-dare.component';
 import {DeepQuestionsComponent} from './games/deep-questions/deep-questions.component';
+import {LandingPageComponent} from './pages/landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: LandingPageComponent },
   { path: 'truth-or-dare', component: TruthOrDareComponent },
   { path: 'deep-questions', component: DeepQuestionsComponent },
+  { path: 'games', component: HomeComponent },
 ];
