601411e03af0fce014ce55146a441ddef9ccf571
sshabtay <shay.shabshay@gmail.com>
2025-03-31 10:50:55 +0300

    music

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { GamesMenuComponent } from './games-menu/games-menu.component';
 import { TruthOrDareComponent } from './games/truth-or-dare/truth-or-dare.component';
 import {DeepQuestionsComponent} from './games/deep-questions/deep-questions.component';
 import {LandingPageComponent} from './pages/landing-page/landing-page.component';
+import {MusicComponent} from './music/music.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'truth-or-dare', component: TruthOrDareComponent },
   { path: 'deep-questions', component: DeepQuestionsComponent },
   { path: 'games', component: GamesMenuComponent },
+  { path: 'music', component: MusicComponent },
 ];
